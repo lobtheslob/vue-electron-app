a22b349148caeac564809bd64bd48743ef18bbb8
Tyler.Simpson <Tyler.Simpson@TSIMPSON19470>
2021-02-28 20:10:59 -0700

    webpack probs - TS

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    transpileDependencies: [
+        'vuetify'
+    ],
+    "pluginOptions": {
+        "electronBuilder": {
+            "nodeIntegration": true, // this may or may not be necessary - you can try without it
+            externals: ['node-forge', 'win-ca'] // this excludes the node-pty from the front end
+        }
+    }
 }
